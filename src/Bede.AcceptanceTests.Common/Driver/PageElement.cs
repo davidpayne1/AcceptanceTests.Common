db8f8e84727bd3b788e89641d6a4f21e368a873c
davidpayne1 <david.payne@bedegaming.com>
2015-06-23 09:47:13 +0100

    initial commit

---
+++ src/Bede.AcceptanceTests.Common/Driver/PageElement.cs
@@ -0,0 +1,14 @@
+﻿namespace Bede.AcceptanceTests.Common.Driver
+{
+        public class PageElement
+        {
+            public string Locator { get; private set; }
+            public PageElementLocatorTypes LocatorType { get; private set; }
+
+            public PageElement(string elementLocator, PageElementLocatorTypes elementLocatorType)
+            {
+                Locator = elementLocator;
+                LocatorType = elementLocatorType;
+            }
+        }
+}
